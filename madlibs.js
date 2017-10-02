5d047120650a7379b1f3f09941a5ec9c9fa71bc0
nmsadoques17 <32141075+nmsadoques17@users.noreply.github.com>
2017-10-02 11:16:42 -0400

    significant change

--- madlibs.js
+++ madlibs.js
@@ -1,18 +1,37 @@
+//initialize variables
 var sentence =[];
 var replacements =[];
 var index=0 ;
+//initialize users
 alert("Filler, Go Away.");
 alert ("End with Shift+6(^)");
-//populate(index,sentence);
-alert (populate (index, sentence));
+//fill sentence
+populate(index,sentence,"word");
+alert (sentence);
+index=0;
+//specify positions
+populate(index,replacements,"position");
+alert (replacements);
+//specify part of speech
+index =0;
+replace(index, sentence, replacements, "part of speech");
+alert(sentence);
 
-function populate(sindex,sarray){
+
+function populate(sindex,sarray,label){
 	do {
-		sarray[sindex]=prompt("Next word?");
+		sarray[sindex]=prompt("Next "+label+"?");
 		sindex = sindex+1;
 	}
 	while (sarray[sindex-1]!="^");
 	console.log(sarray);
 	sarray.splice(-1,1);
 	return sarray;
+}
+
+function replace(sindex, sarray, rarray, label) {
+	for (sindex = 0; sindex < rarray.length; sindex++) {
+		sarray[rarray[sindex]] = prompt("next"+label+"?");
+	}
+	return sarray
 }
